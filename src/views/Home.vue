81d0f326bc53aade8f2bee23172ac907de7201f8
hschae <openaid.chae@gmail.com>
2023-09-05 16:56:37 +0900

    add: tab event

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,45 +6,10 @@ import List from '@editorjs/list'
 import SimpleImage from '@/plugins/simple-image'
 
 const output = ref('')
+const editor = ref(null)
+const test_el = ref(null)
 
 const isSlash = (event) => '/' === event.detail?.target.holder.innerText
-
-const editor = new EditorJS({ 
-  // holder: 'editorjs', 
-  autofocus: true,
-  placeholder: "명령어는 '/' 입력",
-  tools: { 
-    header: Header, 
-    list: List,
-    image: SimpleImage, 
-  }, 
-  onChange: (api, event) => {
-    // console.log('api:', api)
-    // console.log('event:', event)
-    const s = isSlash(event)
-    // console.log('s:', s)
-  },
-  // on: (e, t) => {
-  //   console.log('on e:', e)
-  //   console.log('on t:', t)
-  // },
-  // data: {
-  //   time: 1552744582955,
-  //   blocks: [
-  //     {
-  //       type: "image",
-  //       data: {
-  //         url: "https://cdn.pixabay.com/photo/2017/09/01/21/53/blue-2705642_1280.jpg"
-  //       }
-  //     }
-  //   ],
-  //   version: "2.11.10"
-  // }
-})
-
-
-
-
 
 const save = () => {
   editor.save().then( savedData => {
@@ -61,20 +26,72 @@ const clear = () => {
   editor.clear()
 }
 
-onMounted(() => {
-  console.log('editor:', editor)
+const test = () => {
+  window.alert('aaa')
+}
 
-  // editor.on(('#editorjs', 'keydown', () => {
-  //   console.log('keydown')
-  // }))
+onMounted(() => {
+  // const evt2 = new KeyboardEvent('keydown', {
+  //   code: 'Enter'
+  // })
+  // console.log('test_el:', test_el.value)
+  // test_el.value.focus()
+  // test_el.value.dispatchEvent(evt2)
+
+  const editorjs = new EditorJS({ 
+    // holder: 'editorjs', 
+    // autofocus: true,
+    placeholder: "명령어는 '/' 입력",
+    tools: { 
+      header: Header, 
+      list: List,
+      image: SimpleImage, 
+    }, 
+    onChange: (api, event) => {
+      // console.log('api:', api)
+      // console.log('this:', this) // undefined
+      console.log('event:', event)
+      const s = isSlash(event)
+      if (s) {
+        const evt = new KeyboardEvent('keydown', {
+          code: 'Tab'
+        })
+        console.log('evt:', evt)
+        // console.log('holder:', event.detail.target.holder)
+        // event.detail.target.holder.dispatchEvent(evt)
+        // event.detail.target.holder.innerText = 'wow'
+        // editor.value.dispatchEvent(evt)
+
+      }
+      // console.log('s:', s)
+    },
+    // on: (e, t) => {
+    //   console.log('on e:', e)
+    //   console.log('on t:', t)
+    // },
+    // data: {
+    //   time: 1552744582955,
+    //   blocks: [
+    //     {
+    //       type: "image",
+    //       data: {
+    //         url: "https://cdn.pixabay.com/photo/2017/09/01/21/53/blue-2705642_1280.jpg"
+    //       }
+    //     }
+    //   ],
+    //   version: "2.11.10"
+    // }
+  })
+
 })
 </script>
 
 <template>
-<div id="editorjs"></div>
+<div ref="editor" id="editorjs"></div>
 <div>
   <button @click="save2">저장</button>
   <button @click="clear">클리어</button>
+  <button @click="test" ref="test_el">테스트</button>
 </div>
 <div>{{ output }}</div>
 </template>
